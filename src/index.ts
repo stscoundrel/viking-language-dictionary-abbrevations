524704c4db7f5f1e450fe1e6f5c354b69b1ce5a9
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-07-22 13:55:46 +0300

    Add abbr finder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-export const example = (): string => 'test string';
+import { getAbbreviationMapping } from './mappings/abbreviation-mapping';
 
+export type StringToStringMap = Map<string, string>;
+
+export const findFromMapping = (
+  content: string,
+  abbreviations: StringToStringMap,
+) : StringToStringMap => {
+  const result: StringToStringMap = new Map();
+
+  abbreviations.forEach((value, key) => {
+    // Case sensitive on purpose -> some abbrs should only catch upper cases.
+    if (content.includes(key)) {
+      result.set(key, value);
+    }
+  });
+
+  return result;
+};
+
+export const findAbbreviations = (content: string) : StringToStringMap => {
+  const abbrMap = getAbbreviationMapping();
+  return findFromMapping(content, abbrMap);
+};
 export default {
-  example,
+  getAbbreviationMapping,
+  findAbbreviations,
 };
